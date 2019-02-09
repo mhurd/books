51674e31abe4cd9ce5f1b80814661d299abb2c19
Mike Hurd <mhurd@users.noreply.github.com>
2019-02-09 20:44:59 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -78,13 +78,9 @@
                          }}]}
 
   :main books.main
+  :aot [books.main]
  
   :warnings false
-  
-  :deploy-repositories [["releases" {:url "http://nexus.home:8082/repository/maven-releases/"
-                                     :username "jenkins-build" :password "jenkins-build" }]
-                        ["snapshots" {:url "http://nexus.home:8082/repository/maven-snapshots/"
-                                      :username "jenkins-build" :password "jenkins-build"}]]
 
   :profiles
     {:dev {:dependencies [[ring-mock "0.1.5"]
