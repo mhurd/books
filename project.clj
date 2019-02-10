61a515f874e0c73f7b4ae23a71480b16062c2521
Mike Hurd <mhurd@users.noreply.github.com>
2019-02-10 12:31:19 +0000

    Add mirrors and repos
    its not appearing to work set in the profiles.clj

--- project.clj
+++ project.clj
@@ -79,6 +79,14 @@
 
   :main books.main
   :aot [books.main]
+  
+  :mirrors {#".+" {:url "http://nexus.home:8082/repository/maven-group/"}}
+  :repositories [["snapshots" {:id "neux-home-snapshots" 
+                               :url "http://nexus.home:8082/repository/maven-snapshots"}]
+                 ["releases" {:id "nexus-home-releases"
+                              :url "http://nexus.example.com:8081/nexus/content/repositories/releases" 
+                              :sign-releases false}]]}
+  :auth {:repository-auth {#"nexus.home" {:username "jenkins-build" :password "jenkins-build"}}}
  
   :warnings false
 
