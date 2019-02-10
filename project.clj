12b2b7595848764c7a74fdc780a08abc3edc9012
Mike Hurd <mhurd@users.noreply.github.com>
2019-02-10 12:28:32 +0000

    Try updating the versions again

--- project.clj
+++ project.clj
@@ -11,16 +11,16 @@
 
   :source-paths ["src" "src/books"]
 
-  :dependencies [[compojure "1.3.1"]
-                 [ring "1.3.2"]
-                 [org.clojure/clojurescript "0.0-2511"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [om "0.8.0-beta5"]
+  dependencies [[compojure "1.6.1"]
+                 [ring "1.7.1"]
+                 [org.clojure/clojurescript "1.10.516"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]
+                 [om "0.8.0-rc1"]
                  [http-kit "2.3.0"]
-                 [cljs-ajax "0.3.3"]
+                 [cljs-ajax "0.8.0"]
                  [com.cemerick/url "0.1.1"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "0.5.0-alpha"]
                  [log4j/log4j "1.2.17"
                   :exclusions [javax.mail/mail
                                javax.jms/jms
@@ -28,26 +28,26 @@
                                com.sun.jmx/jmxri]]
                  [log4j/apache-log4j-extras "1.2.17"]
                  [org.slf4j/slf4j-log4j12 "1.7.9"]
-                 [jayq "2.5.2"]
+                 [jayq "2.5.5"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.json "0.2.5"]
-                 [secretary "1.2.1"]                        ;; see http://spootnik.org/entries/2014/10/26_from-angularjs-to-om-a-walk-through.html
-                 [sablono "0.2.22"]                         ;; see https://github.com/r0man/sablono
-                 [prismatic/om-tools "0.3.6"]
-                 [com.cognitect/transit-clj "0.8.259"]      ;; see https://github.com/cognitect/transit-format
-                 [com.cognitect/transit-cljs "0.8.194"]
-                 [com.novemberain/monger "2.0.0"]           ;; see http://clojuremongodb.info/articles/getting_started.html
-                 [clojurewerkz/quartzite "2.0.0"]           ;; see http://clojurequartz.info/articles/getting_started.html
+                 [secretary "1.2.3"]                        ;; see http://spootnik.org/entries/2014/10/26_from-angularjs-to-om-a-walk-through.html
+                 [sablono "0.8.5"]                         ;; see https://github.com/r0man/sablono
+                 [prismatic/om-tools "0.5.0"]
+                 [com.cognitect/transit-clj "0.8.313"]      ;; see https://github.com/cognitect/transit-format
+                 [com.cognitect/transit-cljs "0.8.256"]
+                 [com.novemberain/monger "3.5.0"]           ;; see http://clojuremongodb.info/articles/getting_started.html
+                 [clojurewerkz/quartzite "2.1.0"]           ;; see http://clojurequartz.info/articles/getting_started.html
                  ]
 
-  :plugins [[lein-ring "0.8.13"]
+  :plugins [[lein-ring "0.12.4"]
             [lein-cljsbuild "1.1.7"]
             ;; lein marg --dir resources/public/docs src/gridlife/gamemodel.cljs
-            [lein-marginalia "0.8.0"]
+            [lein-marginalia "0.9.1"]
             ;; https://github.com/weavejester/cljfmt/
             ;; lein cljfmt check, lein cljfmt fix
-            [lein-cljfmt "0.1.10"]]
+            [lein-cljfmt "0.6.4"]]
 
   :hooks [leiningen.cljsbuild]
 
