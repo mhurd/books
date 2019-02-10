c4bdf465b2942422feb8451eaebd4b1c735e47e9
Mike Hurd <mhurd@users.noreply.github.com>
2019-02-10 12:32:18 +0000

    Fix unmatched curly

--- project.clj
+++ project.clj
@@ -85,7 +85,7 @@
                                :url "http://nexus.home:8082/repository/maven-snapshots"}]
                  ["releases" {:id "nexus-home-releases"
                               :url "http://nexus.example.com:8081/nexus/content/repositories/releases" 
-                              :sign-releases false}]]}
+                              :sign-releases false}]]
   :auth {:repository-auth {#"nexus.home" {:username "jenkins-build" :password "jenkins-build"}}}
  
   :warnings false
