5b912626a90bfbc1bf72a4dd5121ba4b248a81c8
Mike Hurd <mhurd@users.noreply.github.com>
2019-04-07 15:43:29 +0100

    Cast sleep time to number

--- src/books/main.clj
+++ src/books/main.clj
@@ -11,7 +11,7 @@
   (let [access-key (first args)
         associate-tag (second args)
         secret (second (rest args))
-        api-sleep (second (rest (rest args)))
+        api-sleep (Integer/parseInt (second (rest (rest args))))
         handler (handler/get-handler access-key associate-tag secret api-sleep)
         scheduler (-> (qs/initialize) qs/start)]
     (log/info "Starting main...")
